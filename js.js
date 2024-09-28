9bef3accbf5931d94415e9cdd10a31c8fc705122
Rohit S <156643465+rohitsethu@users.noreply.github.com>
2024-09-28 13:11:20 +0400

    Update js.js

--- js.js
+++ js.js
@@ -1,12 +1,17 @@
+// Wait for the page to fully load before applying the background image
+window.addEventListener('load', () => {
+    const banner = document.querySelector('.banner');
+    banner.style.backgroundImage = 'url(https://www.simplilearn.com/ice9/free_resources_article_thumb/What_is_the_Importance_of_Technology.jpg)';
+});
+
 // Interactivity for the banner: change background image on hover
 const banner = document.querySelector('.banner');
 
-// When mouse hovers over the banner, change the background image
 banner.addEventListener('mouseover', () => {
-    banner.style.backgroundImage = 'url(https://drive.google.com/file/d/1eBICPpEIRld3BB36MwE0NJ5oAxHgja5d/view?usp=drive_link)';
+    banner.style.backgroundImage = 'url(https://bondartech.com/wp-content/uploads/modernTechnology_620977929_400-1.jpg)';
 });
 
-// When the mouse leaves the banner, change it back to the original image
 banner.addEventListener('mouseout', () => {
-    banner.style.backgroundImage = 'url(https://drive.google.com/file/d/1eBICPpEIRld3BB36MwE0NJ5oAxHgja5d/view?usp=drive_link)';
+    banner.style.backgroundImage = 'url(https://www.simplilearn.com/ice9/free_resources_article_thumb/What_is_the_Importance_of_Technology.jpg)';
 });
+

9ada9df24ff36588b16f5d2f2c51161394c54c4e
Rohit S <156643465+rohitsethu@users.noreply.github.com>
2024-09-28 13:58:03 +0400

    Update js.js

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 // Wait for the page to fully load before applying the background image
 window.addEventListener('load', () => {
     const banner = document.querySelector('.banner');
-    banner.style.backgroundImage = 'url(https://t4.ftcdn.net/jpg/05/79/54/53/360_F_579545387_6JuKZXKyBuvrGTVxcCIXPZnE5cr41vC9.jpg)'; // Update with your image name
+    banner.style.backgroundImage = 'url(C:\Users\Sethu\Downloads\neutral-watercolor-background_23-2150564038.avif)'; // Update with your image name
 });

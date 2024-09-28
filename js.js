4cb0978f033516a29a2696ed451b4ac979f40462
Rohit S <156643465+rohitsethu@users.noreply.github.com>
2024-09-28 13:51:07 +0400

    Update js.js

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 // Wait for the page to fully load before applying the background image
 window.addEventListener('load', () => {
     const banner = document.querySelector('.banner');
-    banner.style.backgroundImage = 'url(https://drive.google.com/file/d/1eBICPpEIRld3BB36MwE0NJ5oAxHgja5d/view?usp=drive_link)'; // Update with your image name
+    banner.style.backgroundImage = 'url(https://t4.ftcdn.net/jpg/05/79/54/53/360_F_579545387_6JuKZXKyBuvrGTVxcCIXPZnE5cr41vC9.jpg)'; // Update with your image name
 });
